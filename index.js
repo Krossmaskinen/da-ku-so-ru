abfbeab7abf6a99749cc889aaa0855f8d1bff1d2
Bill Kurtson <bill.kurtson@gmail.com>
2016-04-08 13:50:36 +0200

    add more

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-    res.sendFile('index.html', {root: './public'});
+    res.sendFile('index.html');
 });
 
-app.use(express.static('public'));
-
-app.listen(3000, function() {
-    console.log( 'listen' );
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
